e3cd5856577f49ae85bca2285c27b9c8a6a0e608
Owl-Boy <sharmashubh223@gmail.com>
2022-01-11 16:49:35 +0530

    chapter 1 complete

---
+++ hello_world/hello_cargo/target/debug/deps/hello_cargo-d8611040948f8ea6.d
@@ -0,0 +1,5 @@
+/home/owlyy/Code/rust/learning_rust/hello_world/hello_cargo/target/debug/deps/hello_cargo-d8611040948f8ea6: src/main.rs
+
+/home/owlyy/Code/rust/learning_rust/hello_world/hello_cargo/target/debug/deps/hello_cargo-d8611040948f8ea6.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/owlyy/Code/rust/learning_rust/hello_world/hello_cargo/target/debug/hello_cargo: /home/owlyy/Code/rust/learning_rust/hello_world/hello_cargo/src/main.rs
